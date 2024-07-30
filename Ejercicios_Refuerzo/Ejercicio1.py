bfd9b0fd8c75b94530d113afe81782b81615543f
Yeison Mosquera <142964641+CtrlJason@users.noreply.github.com>
2024-07-30 14:31:40 -0500

    Update Ejercicio1.py

--- Ejercicios_Refuerzo/Ejercicio1.py
+++ Ejercicios_Refuerzo/Ejercicio1.py
@@ -1,6 +1,6 @@
-alimentacion = int(input("Cuanto gasta en alimentacion: "+"$"))
+alimentacion = int(input("Cuanto gasta en alimentación: "))
 transporte = int(input("Cuanto gasta en trasporte: "+"$"))
-entretenimiento = int(input("Cuanto gasta en entretenimiento: "+"$"))
-otros = int(input("Cuanto gasta en otros: "+"$"))
+entretenimiento = int(input("Cuanto gasta en entretenimiento: "))
+otros = int(input("Cuanto gasta en otros: "))
 total = alimentacion+transporte+entretenimiento+otros
-print(f"Su total de gastos de este mes es de: {total}")
+print(f"Su total de gastos de este mes es de: {total}$")
